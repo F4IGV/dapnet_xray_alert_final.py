a5362199882cea97faf787d8d4dfea59a66ab954
Daniel Yoann <64357912+F4IGV@users.noreply.github.com>
2025-12-07 11:55:59 +0100

    Update run_dapnet_xray_alert.bat with folder info
    Added a comment to specify the folder containing the .py file.

--- run_dapnet_xray_alert.bat
+++ run_dapnet_xray_alert.bat
@@ -1,4 +1,5 @@
 @echo off
 REM Lance le script Python pour envoyer la météo solaire sur DAPNET
-cd /d "C:\Scripts\DAPNET"
+cd /d "C:\Scripts\DAPNET"   #choose your folder who contain the .py file
+
 py "dapnet_xray_alert.py" >> "C:\Scripts\DAPNET\dapnet_xray_alert.log" 2>&1
